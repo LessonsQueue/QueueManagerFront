8475345857d60b205cb69a018923191d44296821
Antonio <antony.dovzhenko@gmail.com>
2024-05-15 16:19:22 +0300

    Added queue component

--- src/components/Content.svelte
+++ src/components/Content.svelte
@@ -1,8 +1,23 @@
 <script>
     import Day from './Day.svelte';
+    import Queue from './Queue.svelte';
     import { scheduleData } from '../mockData.js';
 
     const schedule = scheduleData.data.scheduleFirstWeek;
+
+    let isQueueOpen = false;
+    let position = "right";
+
+    function handleQueue(index) {
+        return function() {
+            isQueueOpen = true;
+            position = index < 3 ? "right" : "left";
+        };
+    }
+
+    function closeQueue() {
+        isQueueOpen = false;
+    }
 </script>
 
 <style>
@@ -32,10 +47,11 @@
 
 <div class="content-wrapper">
     <div class="day-container">
-        {#each schedule as day}
+        {#each schedule as day, index}
             <div class="day-item">
-                <Day {day} />
+                <Day {day} on:showQueue="{handleQueue(index)}" />
             </div>
         {/each}
     </div>
+    <Queue position={position} open={isQueueOpen} on:closeQueue="{closeQueue}" />
 </div>
