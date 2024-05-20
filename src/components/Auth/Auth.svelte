f6b6700014a3ef91fc41c5502965ccf131803e91
Antonio <antony.dovzhenko@gmail.com>
2024-05-20 17:06:59 +0300

    Added login component

---
+++ src/components/Auth/Auth.svelte
@@ -0,0 +1,61 @@
+<script>
+    import LoginForm from './LoginForm.svelte';
+    import SignupForm from './SignUpForm.svelte';
+    let showLoginForm = true;
+
+    function handleLogin(event) {
+        console.log('Login data:', event.detail);
+    }
+
+    function handleSignup(event) {
+        console.log('Signup data:', event.detail);
+    }
+
+    function switchToSignup() {
+        showLoginForm = false;
+    }
+
+    function switchToLogin() {
+        showLoginForm = true;
+    }
+</script>
+
+<style>
+    .auth-container {
+        text-align: center; /* Центрування всього контенту */
+        padding: 20px;
+    }
+
+    img.logo {
+        width: 150px; /* Або інший розмір залежно від вашого дизайну */
+        margin-top: 20px; /* Додає відступ між логотипом та формою */
+        border-radius: 50%;
+    }
+
+    button {
+        margin-top: 10px; /* Додає відступ між формою та кнопкою */
+        width: auto; /* Перевизначення стилів кнопки з LoginForm або SignUpForm */
+        padding: 10px 20px;
+        background-color: #00704a;
+        color: white;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+        transition: background-color 0.3s;
+    }
+
+    button:hover {
+        background-color: #005a36;
+    }
+</style>
+
+<div class="auth-container">
+    <img class="logo" src="/src/img/IMlogo.jpg" alt="Group Logo">
+    {#if showLoginForm}
+        <LoginForm on:login={handleLogin}/>
+        <button on:click={switchToSignup}>Don't have an account? Register here</button>
+    {:else}
+        <SignupForm on:signup={handleSignup}/>
+        <button on:click={switchToLogin}>Already have an account? Login here</button>
+    {/if}
+</div>
---
+++ src/components/Auth/LoginForm.svelte
@@ -0,0 +1,54 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+
+    export let email = '';
+    export let password = '';
+    const dispatch = createEventDispatcher();
+
+    function handleSubmit() {
+        dispatch('login', { email, password });
+    }
+</script>
+
+<style>
+    form {
+        max-width: 800px;
+        margin: 50px auto;
+        padding: 20px;
+        background-color: #f3f3f3;
+        border-radius: 8px;
+        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.3)
+    }
+
+    input[type="email"], input[type="password"] {
+        width: 100%;
+        padding: 10px;
+        margin-top: 10px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+    }
+
+    button {
+        width: 100%;
+        padding: 10px;
+        background-color: #00704a;
+        color: white;
+        border: none;
+        border-radius: 4px;
+        margin-top: 20px;
+        cursor: pointer;
+        transition: background-color 0.3s;
+    }
+
+    button:hover {
+        background-color: #00bc7c;
+    }
+</style>
+
+
+<form on:submit|preventDefault={handleSubmit}>
+    <input type="email" bind:value={email} placeholder="Email">
+    <input type="password" bind:value={password} placeholder="Password">
+    <button type="submit">Login</button>
+</form>
