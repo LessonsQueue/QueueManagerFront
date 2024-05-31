b85f5b3ef07c820f6e6f5cf7884e3040d372fe29
Antonio <antony.dovzhenko@gmail.com>
2024-05-31 10:35:38 +0300

    Add routing for approval page

---
+++ src/routes/pendingUsers/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+    import Header from "../../components/Header.svelte";
+    import Footer from "../../components/Footer.svelte";
+    import ApprovalPage from "../../components/ApprovalPage.svelte";
+</script>
+
+<style>
+    body, html {
+        height: 100%;
+        margin: 0;
+    }
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
+
+    main {
+        flex: 1;
+    }
+</style>
+
+<div class="container">
+    <Header />
+    <main>
+        <ApprovalPage />
+    </main>
+    <Footer />
+</div>
