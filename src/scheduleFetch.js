221f759c26c09c860534515291d238430e55c451
thebladehit <boqdanyarmolka@gmail.com>
2024-06-02 16:33:01 +0300

    Add message for fetching schedule

--- src/scheduleFetch.js
+++ src/scheduleFetch.js
@@ -1,4 +1,5 @@
 import { writable } from 'svelte/store';
+import { showMessage } from './messageStore';
 
 export const scheduleData = writable({});
 
@@ -6,8 +7,10 @@ export const fetchSchedule = async () => {
     const url = 'https://api.campus.kpi.ua/schedule/lessons?groupId=28bfbe3d-c77c-4068-b3eb-f617d1a19cfc';
     const response = await fetch(url);
 
-    if (!response.ok)
+    if (!response.ok) {
+        showMessage('error', 'Reload page please');
         throw new Error(`HTTP error! Status: ${response.status}`);
+    }
 
     const data = await response.json();
     scheduleData.set(data);
